a1f7dbbfe30fcef2cceb7ff58fade25fd06c756e
Alvian03 <74562450+Alvian03@users.noreply.github.com>
2020-12-22 13:35:20 +0700

    Add files via upload

---
+++ src/donasi.js
@@ -0,0 +1,17 @@
+const donasi = () => {
+	return `👾 *BABUBOT* 👾	
+
+  Hi👋️
+  
+          *FITUR*
+          
+┏━━━°❀ ❬ 𝗗𝗢𝗡𝗔𝗦𝗜 ❭ ❀°━━━⊱
+┃
+┣➥ *PULSA:* 088221542833
+┃
+┣━━━━━━━━━━━━━━━━━━━━
+┃ *POWERED BY : VIANJIENG*
+┗━━━━━━━━━━━━━━━━━━━━`
+}
+
+exports.donasi = donasi
